6450daef260bb9ae58a19f30ea7996ada95003d3
e <yellowhzp@gmail.com>
2020-02-22 19:42:05 +0800

    1

---
+++ null_to_mp4.py
@@ -0,0 +1,16 @@
+import sys
+import os
+import re
+import time
+
+if __name__ == "__main__":
+    dirs = os.listdir("./")
+    path = os.getcwd()
+    for dire in dirs:
+        if os.path.isfile(dire):
+            if re.search(r".*\.null$", dire):
+                new_name = re.sub(r"\.null", ".mp4", dire)
+                print "rename %s to %s" % (dire, new_name)
+                os.rename(os.path.join(path, dire), os.path.join(path, new_name))
+    # s = raw_input("press any key to end")
+    time.sleep(1)
